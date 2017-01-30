4aa2fbe8c48de4e962c41a3e455464c36ef7ae9c
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-01-30 14:28:47 -0500

    refactored event listeners. Gave names to  anonymous functions as function expressions ourside of event listeners.

--- js/app.js
+++ js/app.js
@@ -9,23 +9,21 @@ const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
 
-// bind event listeners to li tags using event bubbling
-listDiv.addEventListener('mouseover', (event) => {
+const highlightListItems = (event) => {
   if (event.target.tagName === 'LI'){
     event.target.style.color = 'purple';
     event.target.textContent = event.target.textContent.toUpperCase();
   }
-});
+};
 
-listDiv.addEventListener('mouseout', (event) => {
+const unhighlightListItems = (event) => {
   if (event.target.tagName === 'LI'){
     event.target.style.color = 'black';
     event.target.textContent = event.target.textContent.toLowerCase();
    }
-});
+};
 
-
-toggleList.addEventListener('click', () =>{
+const toggleListDisplay = () => {
   if (listDiv.style.display === 'none'){
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
@@ -33,18 +31,18 @@ toggleList.addEventListener('click', () =>{
      toggleList.textContent = 'Show list';   
      listDiv.style.display = 'none';
    }
-});
+};
 
-descriptionButton.addEventListener('click', () => {
+const setListDescription = () => {
   if (descriptionButtonInput.value === ''){
     descriptionButtonInput.value = '';
   } else {
     descriptionButtonP.textContent = descriptionButtonInput.value + ':';
     descriptionButtonInput.value = '';    
   }
-});
+};
 
-addItemButton.addEventListener('click', () => {
+const addItem = () => {
   let li = document.createElement('li');
   if (addItemInput.value === ''){
     addItemInput.value = 'add something'
@@ -58,12 +56,9 @@ addItemButton.addEventListener('click', () => {
       li.textContent = li.textContent.toUpperCase();
     });
   }
-});
+};
 
-  // removes items. If input field is blank, it deletes the last item
-  // if it has an item on the list, it is deleted.
-  // if the item is not on the list, nothing happens
-removeItemButton.addEventListener('click', () => {
+const removeItem = () => {
   // if value is empty space remove last item
   if(addItemInput.value === ''){
   // another way of doing it
@@ -93,30 +88,57 @@ removeItemButton.addEventListener('click', () => {
         addItemInput.style.boxShadow = '0 0 20px tomato';
       }
   }
-});
-  
-// when in focus
-addItemInput.addEventListener('focus', () => {
+};
+
+const inputBoxInFocusHightlight = () => {
   addItemInput.style.outline = 'none';
   addItemInput.style.borderColor = '#9ecaed';
   addItemInput.style.boxShadow = '0 0 10px #9ecaed';
-});
+};
 
-// when out of focus
-addItemInput.addEventListener('blur', () => {
+const inputBoxNoFocusHightlight = () => {
     addItemInput.style.outline = 'none';
     addItemInput.style.border = '1px solid #dcdcdc';
     addItemInput.style.boxShadow = 'none';
-});
-  
+}
+
 // if empty, sets input field back to normal 
-addItemInput.addEventListener('keyup', () => {
+const inputBoxBackToNormal = () => {
   if(addItemInput === ''){
     addItemInput.style.outline = 'none';
     addItemInput.style.border = '1px solid #dcdcdc';
     addItemInput.style.boxShadow = 'none';
   }
-});
+};
+
+
+// Using event bubbling to binding event to parent list div, so that you can bind event listeners to all li tags.
+// This way it takes up less memory and applies to all new li tags after being created 
+listDiv.addEventListener('mouseover', highlightListItems);
+
+listDiv.addEventListener('mouseout', unhighlightListItems);
+
+toggleList.addEventListener('click', toggleListDisplay);
+
+descriptionButton.addEventListener('click', setListDescription);
+
+// add Item.
+// when mouse over, items turn purple and upper case
+addItemButton.addEventListener('click', addItem);
+
+// removes items. If input field is blank, it deletes the last item
+// if it has an item on the list, it is deleted.
+// if the item is not on the list, box is turned red
+removeItemButton.addEventListener('click', removeItem);
+  
+// when in focus
+addItemInput.addEventListener('focus', inputBoxInFocusHightlight);
+
+// when out of focus
+addItemInput.addEventListener('blur', inputBoxNoFocusHightlight);
+
+// if empty, sets input field back to normal   
+addItemInput.addEventListener('keyup', inputBoxBackToNormal);
   
 
   
